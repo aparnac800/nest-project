e09bbcd61e90ace790ef43d18dfc1b918d6cbbbc
aparnac800 <caparna800@gmail.com>
2025-03-20 07:46:10 +0530

    first

---
+++ src/app/ui/home-discription/home-discription.component.ts
@@ -0,0 +1,11 @@
+import { Component, ElementRef, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-discription',
+  imports: [],
+  templateUrl: './home-discription.component.html',
+  styleUrl: './home-discription.component.css'
+})
+export class HomeDiscriptionComponent {
+
+}
